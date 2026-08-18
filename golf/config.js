72a1de9fa3da1127a419be7252144e8237923ce2
Kyle Beggan <beggank@gmail.com>
2026-08-18 16:38:15 -0400

    Switch PayPal back to sandbox mode and add guidance text

--- golf/config.js
+++ golf/config.js
@@ -5,5 +5,6 @@ window.ENV = {
   SUPABASE_ANON_KEY: "sb_publishable_cCtMmN3C38pQs2ttuMxR5A_pv9XU4hy",
 
   // PayPal Merchant Client ID ("sb" acts as the developer testing sandbox environment)
-  PAYPAL_CLIENT_ID: "BAAo66PnGNcmo_CNkkyu3_aAHf-y4OjaPsmlVg4_ZVAcqb4SywGGm1Yut0mWFhuobG5NRbWn5V0P3GF-cE"
+  // PAYPAL_CLIENT_ID: "BAAo66PnGNcmo_CNkkyu3_aAHf-y4OjaPsmlVg4_ZVAcqb4SywGGm1Yut0mWFhuobG5NRbWn5V0P3GF-cE"
+  PAYPAL_CLIENT_ID: "sb"
 };
