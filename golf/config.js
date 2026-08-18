c488d2a6c6c8acf074f122d03903165a63514bcd
Kyle Beggan <beggank@gmail.com>
2026-08-18 15:18:28 -0400

    Update PayPal Client ID to live

--- golf/config.js
+++ golf/config.js
@@ -5,5 +5,5 @@ window.ENV = {
   SUPABASE_ANON_KEY: "sb_publishable_cCtMmN3C38pQs2ttuMxR5A_pv9XU4hy",
 
   // PayPal Merchant Client ID ("sb" acts as the developer testing sandbox environment)
-  PAYPAL_CLIENT_ID: "AX1yRTSMjPS_asi5XKTEUZvGe-vxpvrcevib19BkdWfLxmVZthVu3loRo7sjv3EoEWXB4zG_L_MH96Y8"
+  PAYPAL_CLIENT_ID: "BAAo66PnGNcmo_CNkkyu3_aAHf-y4OjaPsmlVg4_ZVAcqb4SywGGm1Yut0mWFhuobG5NRbWn5V0P3GF-cE"
 };
